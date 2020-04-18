3df97c219d56f567c3f9dbc8c289ac0ddb3cdd00
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-04-18 12:53:25 +0530

    modified split to use get_custom_string array instead of resize_array and removed resize_array

--- string.c
+++ string.c
@@ -9,28 +9,11 @@ String_array_ptr init_string_array(size_t length)
   return string_array;
 }
 
-String_array_ptr resize_array(String_array_ptr string_array, size_t length)
-{
-  String_array_ptr resized_array = malloc(sizeof(String_array));
-  resized_array->length = length;
-  resized_array->strings = realloc(string_array->strings, (8 * length));
-
-  if (resized_array->strings == NULL)
-  {
-    NO_MEMORY_ERROR;
-    exit(1);
-  }
-
-  // free(string_array->strings);
-  free(string_array);
-  return resized_array;
-}
-
 String_array_ptr get_custom_string_array(Char_ptr *strings, size_t length)
 {
   String_array_ptr custom_string_array = malloc(sizeof(String_array));
   custom_string_array->length = length;
-  custom_string_array->strings = realloc(strings, length);
+  custom_string_array->strings = realloc(strings, (sizeof(Char_ptr) * length));
 
   if (custom_string_array->strings == NULL)
   {
@@ -86,21 +69,21 @@ Char_ptr slice(Char_ptr string, size_t from, size_t to)
 String_array_ptr split(Char_ptr string, char delimiter)
 {
   size_t length_of_string = count_chars(string);
-  String_array_ptr result = init_string_array(length_of_string);
+  Char_ptr *result = malloc(sizeof(Char_ptr) * length_of_string);
   size_t prev_index = 0, result_index = 0;
 
   for (size_t i = 0; i < length_of_string; i++)
   {
     if (string[i] == delimiter)
     {
-      result->strings[result_index] = slice(string, prev_index, i);
+      result[result_index] = slice(string, prev_index, i);
       prev_index = i + 1;
       ++result_index;
     }
   }
 
-  result->strings[result_index] = slice(string, prev_index, length_of_string);
+  result[result_index] = slice(string, prev_index, length_of_string);
   ++result_index;
 
-  return resize_array(result, result_index);
+  return get_custom_string_array(result, result_index);
 }
